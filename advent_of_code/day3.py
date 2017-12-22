a10cc76dc65ff31cc677f53168fd560e749ecfc8
dmcnavish <dave8051@gmail.com>
2017-12-21 21:27:51 -0800

    added day 3

--- advent_of_code/day3.py
+++ advent_of_code/day3.py
@@ -1,6 +1,8 @@
 
 import math
 
+PART_2_TARGET = 289326
+
 
 class Point (object):
     x = 0
@@ -39,7 +41,7 @@ def generate_stress_grid(width, height):
     for i in range(max(height, width)**2):
         if (-width/2 < x <= width/2) and (-height/2 < y <= height/2):
             point = Point(x, y, 1)
-            find_adjacent(grid, point)
+            point.value = find_adjacent_sum(grid, point)
             grid[i+1] = point
         if x == y or (x < 0 and x == -y) or (x > 0 and x == 1-y):
             dx, dy = -dy, dx
@@ -49,23 +51,42 @@ def generate_stress_grid(width, height):
 
 
 def find_adjacent_sum(grid, point):
+    if point.x == 0 and point.y == 0:
+        return 1
+
     sum = 0
-    print "point: " + point.to_string()
-    print_point_grid(grid)
-
     points_surrounding = []
-    points_surrounding.append(Point(point.x + 1, point.y))
-    points_surrounding.append(Point(point.x, point.y+1))
-    points_surrounding.append(Point(point.x+1, point.y+1))
+    points_surrounding.append(find_point_at(grid, point.x + 1, point.y))
+    points_surrounding.append(find_point_at(grid, point.x, point.y+1))
+    points_surrounding.append(find_point_at(grid, point.x+1, point.y+1))
 
-    points_surrounding.append(Point(point.x-1, point.y))
-    points_surrounding.append(Point(point.x, point.y-1))
-    points_surrounding.append(Point(point.x-1, point.y-1))
+    points_surrounding.append(find_point_at(grid, point.x-1, point.y))
+    points_surrounding.append(find_point_at(grid, point.x, point.y-1))
+    points_surrounding.append(find_point_at(grid, point.x-1, point.y-1))
 
-    points_surrounding.append(Point(point.x+1, point.y-1))
-    points_surrounding.append(Point(point.x-1, point.y+1))
+    points_surrounding.append(find_point_at(grid, point.x+1, point.y-1))
+    points_surrounding.append(find_point_at(grid, point.x-1, point.y+1))
+
+    for neighbor in points_surrounding:
+        if neighbor is None:
+            continue
+        sum += neighbor.value
+
+    if sum > PART_2_TARGET:
+        print "first value greater: " + str(sum)
+        exit()
 
     return sum
+
+
+def find_point_at(grid, x, y):
+    for point in grid:
+        if point == 0:
+            continue
+        if point.x == x and point.y == y:
+            return point
+
+    return None
 
 
 def find_distance(grid, position_index):
@@ -74,20 +95,23 @@ def find_distance(grid, position_index):
 
 
 def print_point_grid(grid):
-    print "grid: "
     for x in range(1, len(grid)):
+        if grid[x] == 0:
+            continue
         print grid[x].to_string()
+
 
 def main():
     # size = 289326
-    # width = int(math.sqrt(size)) + 1
-    # height = int(math.sqrt(size)) + 1
-    # grid = generate_grid(width,height)
-    #
-    # distance = find_distance(grid, 289326)
-    # print "part1 distance: " + str(int(distance))
+    size = 26
+    width = int(math.sqrt(size)) + 1
+    height = int(math.sqrt(size)) + 1
+    grid = generate_grid(width,height)
+    print "grid: " + str(grid)
+    distance = find_distance(grid, 23) #289326
+    print "part1 distance: " + str(int(distance))
 
-    size = 25
+    size = 289326
     width = int(math.sqrt(size)) + 1
     height = int(math.sqrt(size)) + 1
     grid = generate_stress_grid(width,height)
--- tests/test_day3.py
+++ tests/test_day3.py
@@ -28,13 +28,33 @@ class Day3Tests(unittest.TestCase):
         self.assertEqual(result, 31)
 
     def test_find_adjacent_sum(self):
-        grid = [0, Point(0,0,1)]
-        result = find_adjacent_sum(grid, Point(1,0,0))
-        self.assertEqual(1,result)
+        grid = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+        result = find_adjacent_sum(grid, Point(0,0,0))
+        self.assertEqual(1, result)
 
-        grid = [0, Point(0,0,1), Point(1,0,1)]
+        grid = [0, Point(0,0,1), 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+        result = find_adjacent_sum(grid, Point(1,0,0))
+        self.assertEqual(1, result)
+
+        grid = [0, Point(0,0,1), Point(1,0,1), 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
         result = find_adjacent_sum(grid, Point(1,1,0))
-        self.assertEqual(2,result)
+        self.assertEqual(2, result)
+
+        grid = [0, Point(0,0,1), Point(1,0,1), Point(1,1,2), 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+        result = find_adjacent_sum(grid, Point(0,1,0))
+        self.assertEqual(4, result)
+
+        grid = [0, Point(0,0,1), Point(1,0,1), Point(1,1,2), Point(0,1,4), 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+        result = find_adjacent_sum(grid, Point(-1,1,0))
+        self.assertEqual(5, result)
+
+        grid = [0, Point(0,0,1), Point(1,0,1), Point(1,1,2), Point(0,1,4), Point(-1,1,5), 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+        result = find_adjacent_sum(grid, Point(-1,0,0))
+        self.assertEqual(10, result)
+
+        grid = [0, Point(0,0,1), Point(1,0,1), Point(1,1,2), Point(0,1,4), Point(-1,1,5), Point(-1,0,10), 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+        result = find_adjacent_sum(grid, Point(-1,-1,0))
+        self.assertEqual(11, result)
 
 
 def main():
